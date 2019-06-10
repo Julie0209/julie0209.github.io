ac4d4ff6eba574389a28b1e01e79889543ab2fd8
Julie0209 <51585016+Julie0209@users.noreply.github.com>
2019-06-10 14:02:06 +0800

    Add files via upload

---
+++ VitA2.php
@@ -0,0 +1,344 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>VitA2</title>
+
+	<style type="text/css">
+
+        .bg {
+            position: fixed;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            z-index: -999;
+       		}
+        .bg img {
+            min-height: 100%;
+            min-width: 1000px;
+            width: 100%;
+        	}
+
+        @media screen and (max-width: 1000px) {
+            img.bg {
+                left: 50%;
+                margin-left: -500px;
+            }
+        }    
+
+
+ #sitebody{
+    
+    padding: 0;
+    margin: 0 auto;　
+    text-align: center;
+
+    }
+
+#head{
+    
+    margin: 0 auto;
+    width: 100%;
+    height:800px;　
+    text-align: 160px;
+    line-height:200px; 
+
+    }
+
+#sidebar{
+    
+    background-color: #ffd382;
+    width:100%;　
+    height:500px;　
+    text-align:left;　
+    line-height:400px;
+    font-size:20px;
+    font-weight:bold;
+    border-color:#cccc00;
+    border-top-style: dotted;
+    border-bottom-style: dotted; 
+    
+    }
+
+#content{
+
+    height:400px;　
+    width: 100%;
+    text-align:center;　
+    line-height:400px;
+
+    }
+
+#footer{
+
+    width: 100%;
+    bottom: 0; 
+    background-color:#FFD1A1; 
+    padding:0.5px; 
+    text-align:center;　
+    line-height:80px; 
+    position: fixed;
+    font-size: 13px;
+
+    }
+
+
+#button{
+	
+	position: relative; z-index: 1;
+    border: 2px solid #555555; 
+    border-radius: 10px ; 
+    background-color: #fff0d4; 
+    padding-left: 40px;
+    padding-right: 8px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    text-align: center;
+    float: left;    
+    margin-left: 50px: 
+
+}
+
+#pic{
+
+	float: left;
+	text-align: center;
+	position: relative;
+	z-index: 2;
+	left: 40px;
+	top: -10px;
+
+}
+
+
+
+a:link{
+    
+    color:#805300;
+    text-decoration:none;
+    font-family: Microsoft JhengHei;
+}
+
+a:visited{
+
+    color:#ffac12;
+
+}
+
+a:hover {
+
+    text-decoration:none;
+    color:#ff1c1c;
+
+}
+
+a:active {
+
+    text-decoration:none;
+    color:#000000;
+
+}
+#int{
+		
+		width: 70%;
+	//	background-color: #ffffff; 
+		text-align: left;
+		font-size: 50px;
+
+		}
+
+	#int1{
+
+		  width: 15%; 
+	//    background-color: #fcfcfc;
+		  float: left; 
+		 }
+		 
+	#pic1{
+
+		//background-color: #f55fff;
+		width: 30%;
+		height: 300px;
+		bottom: 0;
+		float:left;
+
+		top: 70px;
+
+		position: relative;
+		z-index: 3;
+		}
+	#bor1{
+
+		float: left;
+	   // background-color: #f7900f;
+		width: 30%;
+		height: 300px;
+		bottom: 0;
+		text-align: center;
+
+		top: -300px;
+
+		position: relative;
+		z-index: 2;
+		}
+
+	#pic2{
+
+		//background-color: #f55fff;
+		width: 30%;
+		height: 300px;
+		bottom: 0;
+		float: left;
+
+		top: -30px;
+
+		position: relative;
+		z-index: 3;
+		}
+
+	#bor2{
+
+		float: left;
+	   // background-color: #f7900f;
+		width: 30%;
+		height: 300px;
+		bottom: 0;
+		text-align: center;
+
+		top: -400px;
+
+		position: relative;
+		z-index: 2;
+		}
+
+	#A1{
+		position:absolute;
+		top:350px;
+		left:34.5%;
+		z-index:1;
+		}
+	#A2{
+		position:absolute;
+		top:800px;
+		left:20%;
+		z-index:1;
+		}
+	#A3{
+		position:absolute;
+		top:970px;
+		left:15%;
+		z-index:1;
+		}
+	#A4{
+		position:absolute;
+		top:970px;
+		left:28%;
+		z-index:1;
+		}
+	#A5{
+		position:absolute;
+		top:970px;
+		left:55%;
+		z-index:1;
+		}
+
+
+
+
+
+
+</style>
+
+
+
+</head>
+<body>
+
+
+<div class="bg">
+	<img src= "websitetry\進階程式語言\背景.jpg" id="bg-img" style= " padding:0px; " >
+</div>
+
+
+<div id="sitebody">
+
+<div id="header" style="background-color:#bd7b00; font-family: chalkduster; font-size:60px; color:#ffffff; bottom:o; ">
+
+	<div style="float: left; height: 230px;  padding:20px 0px 0px 20px; "> <img src="websitetry\進階程式語言\圖片1.png" style="height: 220px;"> </div>
+
+	<div style="float: left; height: 200px; width: 600px; text-align: center; "> <br><br>New treat ion</div>
+
+	<div style="clear: both;"></div>
+
+
+</div>
+
+<div id="sidebar" style="text-align: center;">
+
+<div style="float: left;"><a href="index.php"> <img src="websitetry\進階程式語言\4.gif"></a> </div>
+
+<br>
+ 
+ 
+ <div id="pic"><img src="websitetry\進階程式語言\2.gif" style="width: : 30px;"></div>
+ <div id="button"><a href="rank.php" target="_blank">熱銷排行榜</a></div> 
+ 
+ <div id="pic" style="top:-20px; left: 50px;"><img src="websitetry\進階程式語言\5.gif"></div>
+ <div id="button" ><a href="howto.php" target="_blank">如何選購與保存</a></div>
+ 
+ <div id="pic" style="top:-15px; left: 50px;"><img src="websitetry\進階程式語言\1.gif" style="width: 55px;"></div>
+ <div id="button" ><a href="intro" target="_blank">食品標章介紹</a></div>
+ 
+ <div id="pic"><img src="websitetry\進階程式語言\3.gif" style="width: : 30px;"></div>
+ <div id="button" ><a href="typeandintro.php" target="_blank">種類與介紹</a></div>
+ 
+ <div id="pic" style="top:-2px; left: 45px;"><img src="websitetry\進階程式語言\6.png"></div>
+ <div id="button" ><a href="qa.php" target="_blank">保健食品Q&A</a></div>
+ 
+ <div id="pic" style="top:-2px; left: 45px;"><img src="websitetry\進階程式語言\6.png"></div>
+ <div id="button" ><a href="talk.php" target="_blank">討論區</a></div>
+
+
+<br><br>
+
+
+ </div>
+
+
+<div id="content"> 
+	<br><br>
+	<div style="text-align: left; height: 10%;"> <a href="VitA.php"><img src="websitetry\進階程式語言\程式語言物件圖檔\圖片9.png" style=" top: -100px;"></a> </div>
+
+	<div id="A1">
+			<img src="websitetry\進階程式語言\1-7\8.png" width="400" height="400">
+		</div>
+		
+		<div id="A2">
+			<font size="10">萊萃美女性專用綜合維生素加鐵100粒<br>價格：600~800</font>
+		</div>
+		
+		<div id="A3">
+			<a href="https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=1296733&Area=search&mdiv=403&oid=1_2&cid=index&kw=%E8%90%8A%E8%90%83%E7%BE%8E%E5%A5%B3%E6%80%A7%E5%B0%88%E7%94%A8%E7%B6%9C%E5%90%88%E7%B6%AD%E7%94%9F%E7%B4%A0%E5%8A%A0%E9%90%B5" target="_blank">
+				<img src="websitetry\進階程式語言\通路1\圖片3.png" width="300" height="300">
+			</a>
+		</div>
+		
+		<div id="A4" style="margin-left:125">
+			<a href="https://www.watsons.com.tw/%E5%A5%B3%E6%80%A7%E7%B6%9C%E5%90%88%E7%B6%AD%E7%94%9F%E7%B4%A0%E5%8A%A0%E9%90%B5100%E9%8C%A0/p/BP_960014?search2=??????????????" target="_blank">
+				<img src="websitetry\進階程式語言\通路1\圖片9.png" width="400" height="180">
+			</a>
+		</div>
+		
+		<div id="A5" style="margin-left:160">
+			<a href="https://tw.buy.yahoo.com/gdsale/%E8%90%8A%E8%90%83%E7%BE%8E-%E5%A5%B3%E6%80%A7%E5%B0%88%E7%94%A8%E7%B6%9C%E5%90%88%E7%B6%AD%E7%94%9F%E7%B4%A0%E5%8A%A0%E9%90%B5%E9%8C%A0-100%E9%8C%A0--6014958.html" target="_blank">
+				<img src="websitetry\進階程式語言\通路1\圖片7.jpg" width="250" height="250">
+			</a>
+		</div>
+		
+<div id="footer"> Copyright  ©  2019 Chung Shan Medical University.<br>websitetry\進階程式語言Semester 2nd 第一組</div>
+
+</div>
+
+
+</body>
+</html>
---
+++ VitE6.php
@@ -0,0 +1,346 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>VitE6</title>
+
+	<style type="text/css">
+
+        .bg {
+            position: fixed;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            z-index: -999;
+       		}
+        .bg img {
+            min-height: 100%;
+            min-width: 1000px;
+            width: 100%;
+        	}
+
+        @media screen and (max-width: 1000px) {
+            img.bg {
+                left: 50%;
+                margin-left: -500px;
+            }
+        }    
+
+
+ #sitebody{
+    
+    padding: 0;
+    margin: 0 auto;　
+    text-align: center;
+
+    }
+
+#head{
+    
+    margin: 0 auto;
+    width: 100%;
+    height:800px;　
+    text-align: 160px;
+    line-height:200px; 
+
+    }
+
+#sidebar{
+    
+    background-color: #ffd382;
+    width:100%;　
+    height:500px;　
+    text-align:left;　
+    line-height:400px;
+    font-size:20px;
+    font-weight:bold;
+    border-color:#cccc00;
+    border-top-style: dotted;
+    border-bottom-style: dotted; 
+    
+    }
+
+#content{
+
+    height:400px;　
+    width: 100%;
+    text-align:center;　
+    line-height:400px;
+
+    }
+
+#footer{
+
+    width: 100%;
+    bottom: 0; 
+    background-color:#FFD1A1; 
+    padding:0.5px; 
+    text-align:center;　
+    line-height:80px; 
+    position: fixed;
+    font-size: 13px;
+
+    }
+
+
+#button{
+	
+	position: relative; z-index: 1;
+    border: 2px solid #555555; 
+    border-radius: 10px ; 
+    background-color: #fff0d4; 
+    padding-left: 40px;
+    padding-right: 8px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    text-align: center;
+    float: left;    
+    margin-left: 50px: 
+
+}
+
+#pic{
+
+	float: left;
+	text-align: center;
+	position: relative;
+	z-index: 2;
+	left: 40px;
+	top: -10px;
+
+}
+
+
+
+a:link{
+    
+    color:#805300;
+    text-decoration:none;
+    font-family: Microsoft JhengHei;
+}
+
+a:visited{
+
+    color:#ffac12;
+
+}
+
+a:hover {
+
+    text-decoration:none;
+    color:#ff1c1c;
+
+}
+
+a:active {
+
+    text-decoration:none;
+    color:#000000;
+
+}
+#int{
+		
+		width: 70%;
+	//	background-color: #ffffff; 
+		text-align: left;
+		font-size: 50px;
+
+		}
+
+	#int1{
+
+		  width: 15%; 
+	//    background-color: #fcfcfc;
+		  float: left; 
+		 }
+		 
+	#pic1{
+
+		//background-color: #f55fff;
+		width: 30%;
+		height: 300px;
+		bottom: 0;
+		float:left;
+
+		top: 70px;
+
+		position: relative;
+		z-index: 3;
+		}
+	#bor1{
+
+		float: left;
+	   // background-color: #f7900f;
+		width: 30%;
+		height: 300px;
+		bottom: 0;
+		text-align: center;
+
+		top: -300px;
+
+		position: relative;
+		z-index: 2;
+		}
+
+	#pic2{
+
+		//background-color: #f55fff;
+		width: 30%;
+		height: 300px;
+		bottom: 0;
+		float: left;
+
+		top: -30px;
+
+		position: relative;
+		z-index: 3;
+		}
+
+	#bor2{
+
+		float: left;
+	   // background-color: #f7900f;
+		width: 30%;
+		height: 300px;
+		bottom: 0;
+		text-align: center;
+
+		top: -400px;
+
+		position: relative;
+		z-index: 2;
+		}
+
+	#A1{
+		position:absolute;
+		top:350px;
+		left:45%;
+		z-index:1;
+		}
+	#A2{
+		position:absolute;
+		top:800px;
+		left:20%;
+		z-index:1;
+		}
+	#A3{
+		position:absolute;
+		top:910px;
+		left:15%;
+		z-index:1;
+		}
+	#A4{
+		position:absolute;
+		top:970px;
+		left:35%;
+		z-index:1;
+		}
+	#A5{
+		position:absolute;
+		top:970px;
+		left:55%;
+		z-index:1;
+		}
+
+
+
+
+
+
+
+</style>
+
+
+
+</head>
+<body>
+
+
+<div class="bg">
+	<img src= "websitetry\進階程式語言\背景.jpg" id="bg-img" style= " padding:0px; " >
+</div>
+
+
+<div id="sitebody">
+
+<div id="header" style="background-color:#bd7b00; font-family: chalkduster; font-size:60px; color:#ffffff; bottom:o; ">
+
+	<div style="float: left; height: 230px;  padding:20px 0px 0px 20px; "> <img src="websitetry\進階程式語言\圖片1.png" style="height: 220px;"> </div>
+
+	<div style="float: left; height: 200px; width: 600px; text-align: center; "> <br><br>New treat ion</div>
+
+	<div style="clear: both;"></div>
+
+
+</div>
+
+<div id="sidebar" style="text-align: center;">
+
+<div style="float: left;"><a href="index.php"> <img src="websitetry\進階程式語言\4.gif"></a> </div>
+
+<br>
+ 
+ 
+ <div id="pic"><img src="websitetry\進階程式語言\2.gif" style="width: : 30px;"></div>
+ <div id="button"><a href="rank.php" target="_blank">熱銷排行榜</a></div> 
+ 
+ <div id="pic" style="top:-20px; left: 50px;"><img src="websitetry\進階程式語言\5.gif"></div>
+ <div id="button" ><a href="howto.php" target="_blank">如何選購與保存</a></div>
+ 
+ <div id="pic" style="top:-15px; left: 50px;"><img src="websitetry\進階程式語言\1.gif" style="width: 55px;"></div>
+ <div id="button" ><a href="intro" target="_blank">食品標章介紹</a></div>
+ 
+ <div id="pic"><img src="websitetry\進階程式語言\3.gif" style="width: : 30px;"></div>
+ <div id="button" ><a href="typeandintro.html" target="_blank">種類與介紹</a></div>
+ 
+ <div id="pic" style="top:-2px; left: 45px;"><img src="websitetry\進階程式語言\6.png"></div>
+ <div id="button" ><a href="qa.php" target="_blank">保健食品Q&A</a></div>
+ 
+ <div id="pic" style="top:-2px; left: 45px;"><img src="websitetry\進階程式語言\6.png"></div>
+ <div id="button" ><a href="talk.php" target="_blank">討論區</a></div>
+
+
+<br><br>
+
+
+ </div>
+
+
+<div id="content"> 
+	<br><br>
+	<div style="text-align: left; height: 10%;"> <a href="VitE.php"><img src="websitetry\進階程式語言\程式語言物件圖檔\圖片9.png" style=" top: -100px;"></a> </div>
+
+	<div id="A1">
+			<img src="websitetry\進階程式語言\1-7\6.png" width="200" height="400">
+		</div>
+		
+		<div id="A2">
+			<font size="10">德國 好立善 維生素C1000＋硒＋E發泡錠／20錠<br>價格：150~250</font>
+		</div>
+		
+		<div id="A3">
+			<a href="https://24h.pchome.com.tw/prod/DBBC0N-A9009BKQU">
+				<img src="websitetry\進階程式語言\通路1\圖片5.png" width="300" height="350">
+			</a>
+		</div>
+		
+		<div id="A4" style="margin-left:125">
+			<a href="https://tw.buy.yahoo.com/gdsale/%E5%BE%B7%E5%9C%8B-%E5%A5%BD%E7%AB%8B%E5%96%84-%E7%B6%AD%E7%94%9F%E7%B4%A0C1000-%E7%A1%92-E%E7%99%BC%E6%B3%A1%E9%8C%A0-20-7112184.html">
+				<img src="websitetry\進階程式語言\通路1\圖片7.jpg" width="230" height="230">
+			</a>
+		</div>
+		
+		<div id="A5" style="margin-left:160">
+			<a href="https://www.rakuten.com.tw/shop/paylessshop/product/0103043/?l-id=tw_search_grid_product_thumbnail_1">
+				<img src="websitetry\進階程式語言\通路1\圖片2.jpg" width="260" height="220">
+			</a>
+		</div>
+	
+			
+<div id="footer"> Copyright  ©  2019 Chung Shan Medical University.<br>websitetry\進階程式語言Semester 2nd 第一組</div>
+
+</div>
+
+
+</body>
+</html>
